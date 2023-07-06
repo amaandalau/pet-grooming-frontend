45af3efbebfc25d4510211e4cb43f5a8b335e3dd
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-06 17:54:14 +0800

    Add logo

---
+++ petlify/src/components/icons/Logo.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+    <img width="32" height="32" src="https://img.icons8.com/pastel-glyph/64/cat--v3.png" alt="cat--v3"/>
+    <a  href="https://icons8.com/icon/121371/cat"/>
+</template>
