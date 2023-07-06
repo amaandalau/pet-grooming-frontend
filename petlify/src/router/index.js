7d22184ad4dce1894003ad8754764e2fee59a3b0
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-06 16:57:27 +0800

    Update routes component import methods

--- petlify/src/router/index.js
+++ petlify/src/router/index.js
@@ -1,17 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/pages/Home.vue'
-import SignUp from '@/pages/SignUp.vue'
+
+// const updateTitle = (to) => {
+//     const baseTitle = 'Petlify'
+
+//     // Set document title based on current route
+//     document.title = to.meta.title ? `${to.meta.title} - ${baseTitle}` : baseTitle
+// }
+
+// router.beforeEach((to, from, next) => {
+//     updateTitle(to) // Update title before each route navigation
+//     next()
+// })
 
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home
+        component: () => import('@/pages/Home.vue')
     },
     {
         path: '/signup',
         name: 'Sign Up',
-        component: SignUp
+       component: () => import('@/pages/SignUp.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('@/pages/Login.vue')
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import('@/pages/About.vue')
     }
 ]
 
