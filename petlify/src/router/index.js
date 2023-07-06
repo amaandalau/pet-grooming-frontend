18e58ad7df871e8a768b96474da00e87edb93c17
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-06 11:26:31 +0800

    Add router

---
+++ petlify/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    {
+        path: '',
+        name: '',
+        component: ''
+    }
+]
+
+const router = createRouter({
+    routes: routes,
+    history: createWebHistory()
+})
+
+export default router
