9797ec2e46d45bdd5ffb440716a0ae7dd542e973
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-06 14:23:27 +0800

    Add footer

--- petlify/src/App.vue
+++ petlify/src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
-import EmptyState from './components/EmptyState.vue';
+import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <EmptyState/>
+  <RouterView/>
+
+  <footer class="fixed bottom-0 left-0 right-0 p-4 border border-slate-800">
+        <div class="text-center">
+            <label class="text-sm font-normal text-gray-600">
+              &copy; {{ new Date().getFullYear() }} All rights reserved | @amaandalau
+            </label>
+        </div>
+  </footer>
 </template>
