f8351c193fc0a8fb873ff2931f7d6fed5f86b7b5
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-06 16:57:10 +0800

    Add navbar styling

---
+++ petlify/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Buttons from './Buttons.vue';
+import Logo from './icons/Logo.vue';
+const props = defineProps({
+
+})
+</script>
+
+<template>
+    <div class="bg-[#BDE0FE] flex flex-row items-center justify-between p-4">
+
+        <div class="flex flex-row justify-start gap-1">
+            <Logo/>
+            <label class="font-semibold text-xl">Petlify</label>
+        </div>
+
+        <div class="flex flex-row justify-evenly gap-4">
+            <slot>
+            </slot>
+        </div>
+
+    </div>
+
+</template>
