7061c56dce29d811711f98a25fd8d9c230ab2204
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-07 16:24:18 +0800

    Add home page

---
+++ petlify/src/pages/Home.vue
@@ -0,0 +1,45 @@
+<script setup>
+import ButtonNew from '../components/ButtonNew.vue';
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
+
+</script>
+
+<template>
+    <Navbar/>
+    
+    <!-- <div class="m-auto text-center flex flex-col">
+        <label class="text-5xl font-bold font-sans">
+            Beautify A Pet
+        </label>
+
+        <label>
+            Groom your pet OR Groom a pet
+        </label>
+
+        <router-link to="/signup">
+            <ButtonNew title="Sign Up Now"/>
+        </router-link>
+
+        <router-link to="/login">
+            <label class="text-sm font-light text-gray-600 hover:cursor-pointer hover:underline">Have an account? Login</label>
+        </router-link>
+    </div> -->
+
+    <div class="flex flex-col justify-center items-center gap-10 min-h-screen max-h-screen">
+        <div class="flex flex-col justify-center items-center gap-2">
+            <label class="text-6xl font-extrabold">
+                Beautify A Pet
+            </label>
+            <label class="text-md font-light">Groom your pet OR Groom a pet, your choice</label>
+        </div>
+
+        <div>
+            <router-link to="/signup">
+                <ButtonNew text="Get Started"/>
+            </router-link>
+        </div>
+    </div>
+
+    <Footer/>
+</template>
