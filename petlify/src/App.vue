05dcd35f06b6ac9c0c40db2ea3719b7fa0ffb179
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-07 13:45:48 +0800

    Remove footer

--- petlify/src/App.vue
+++ petlify/src/App.vue
@@ -6,11 +6,11 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView/>
 
-  <footer class="fixed bottom-0 left-0 right-0 p-4 border border-slate-800">
+  <!-- <footer class="sticky bottom-0 left-0 right-0 p-4 bg-black">
         <div class="text-center">
-            <label class="text-sm font-normal text-gray-600">
+            <label class="text-sm font-normal text-white">
               &copy; {{ new Date().getFullYear() }} All rights reserved | @amaandalau
             </label>
         </div>
-  </footer>
+  </footer> -->
 </template>
