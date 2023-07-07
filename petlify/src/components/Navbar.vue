55b07552fa62df3b217db23fc9100fad2ab4a51e
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-07 13:46:28 +0800

    Update navbar

--- petlify/src/components/Navbar.vue
+++ petlify/src/components/Navbar.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ButtonNew from './ButtonNew.vue';
 import Buttons from './Buttons.vue';
 import Logo from './icons/Logo.vue';
 const props = defineProps({
@@ -7,16 +8,28 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="bg-[#BDE0FE] flex flex-row items-center justify-between p-4">
+    <div class="bg-white flex flex-row items-center justify-between p-4 z-50 drop-shadow-sm">
 
-        <div class="flex flex-row justify-start gap-1">
-            <Logo/>
-            <label class="font-semibold text-xl">Petlify</label>
-        </div>
+        <router-link to="/">
+            <div class="flex flex-row justify-start gap-1 hover:cursor-pointer">
+                <Logo/>
+                <label class="font-semibold text-xl hover:cursor-pointer">Petlify</label>
+            </div>
+        </router-link>
 
         <div class="flex flex-row justify-evenly gap-4">
-            <slot>
-            </slot>
+            <router-link to="/">
+                <Buttons class="link" title="Home"/>
+            </router-link>
+
+            <router-link to="/about">
+                <Buttons class="link" title="About"/>
+            </router-link>
+
+            <router-link to="/signup">
+                <Buttons class="default" title="Sign Up"/>
+                <!-- <ButtonNew text="Sign Up"/> -->
+            </router-link>
         </div>
 
     </div>
