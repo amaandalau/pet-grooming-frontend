619d97cc914dd31eab695b842d2d6ce3d733f04a
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-07 13:46:46 +0800

    Add sign up page

---
+++ petlify/src/pages/SignUp.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Navbar from '../components/Navbar.vue';
+import Input from '../components/Input.vue';
+import InputNew from '../components/InputNew.vue';
+import ButtonNew from '../components/ButtonNew.vue';
+
+</script>
+
+<template>
+    <Navbar/>
+
+    <div class="flex flex-col items-center justify-center gap-12 m-auto w-screen">
+        <div class="flex flex-col text-center gap-2 p-4">
+            <label class="text-3xl font-semibold">Sign Up</label>
+            <label class="text-sm font-light">Let's get started</label>
+        </div>
+
+        <div class="flex flex-col gap-16">
+            <InputNew label="Name"/>
+            <InputNew label="Email"/>
+            <InputNew label="Password"/>
+
+            <div class="flex flex-col justify-center items-center gap-2">
+                <ButtonNew text="Sign Up" rounded="lg"/>
+
+            <label class="font-light text-slate-600">
+                Have an account? 
+                <router-link to="/login">
+                    <label class="text-black hover:underline hover:cursor-pointer">Log in</label>
+                </router-link>
+            </label>
+            </div>
+        </div>
+    </div>
+</template>
