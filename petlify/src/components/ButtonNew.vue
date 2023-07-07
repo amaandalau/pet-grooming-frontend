e3c74493da98f40964e7f9ea423a3f539e953b9d
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-07 13:46:10 +0800

    Add button component

---
+++ petlify/src/components/ButtonNew.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String
+    },
+    rounded: {
+        type: String
+    }
+})
+</script>
+
+<template>
+    <v-btn block :rounded="rounded" size="x-large" class="default">
+        <span class="button-text">{{ text }}</span>
+    </v-btn>
+</template>
+
+<style>
+.default {
+    background: black !important;
+    color: white !important;
+}
+
+.button-text {
+    text-transform: none;
+}
+</style>
