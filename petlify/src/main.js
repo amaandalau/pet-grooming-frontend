2bea21d2a15bbb704f691a093a64994ed83ce0f4
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-08 17:25:46 +0800

    Add sign up form

--- petlify/src/main.js
+++ petlify/src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import '@mdi/font/css/materialdesignicons.css';
+
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -13,6 +15,9 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
   components,
   directives,
+  options: {
+    iconfont: 'mdi'
+  }
 })
 
 const app = createApp(App)
---
+++ petlify/src/pages/SignUpForm.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref } from "vue";
+import Navbar from "../components/Navbar.vue";
+
+const visible = ref(false);
+
+</script>
+
+<template>
+  <Navbar />
+  <div class="mt-8">
+    <v-card class="mx-auto pa-12 pb-8" elevation="0" max-width="448" rounded="lg">
+      <div class="text-center text-2xl font-semibold">Sign Up</div>
+      <div class="text-center text-sm font-light mb-8">Let's get started</div>
+
+      <v-text-field
+        label="Name"
+        density="comfortable"
+        placeholder="Enter your name"
+        variant="outlined"
+        hint="Enter your name"
+        class="mb-4"
+      ></v-text-field>
+
+      <v-text-field
+        label="Email"
+        density="comfortable"
+        placeholder="Enter your email"
+        variant="outlined"
+        hint="Enter your email"
+        class="mb-4"
+      ></v-text-field>
+
+
+      <v-text-field
+        label="Password"
+        hint="Enter your password"
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="comfortable"
+        placeholder="Enter your password"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+        class="mb-4"
+      ></v-text-field>
+
+      <v-select
+        clearable
+        label="Choose A Role"
+        :items="['Pet Owner', 'Pet Groomer']"
+        variant="outlined"
+        density="comfortable"
+      ></v-select>
+
+      <v-btn block class="my-8 bg-black text-white" size="large" variant="outlined"> Sign Up </v-btn>
+
+      <v-card-text class="text-center text-slate-700 font-light">
+        Have an account?
+        <a class="text-black hover:underline" href="/signup" rel="noopener noreferrer">
+          Log in
+        </a>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
