e02e7e4bee8bf0dea83cdc220a92dd2ff9dbafd7
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-10 16:50:06 +0800

    Create services store

---
+++ petlify/src/stores/services.js
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+
+export const useServicesStores = defineStore({
+    id: 'services',
+
+    state: () => {
+
+    },
+
+    getters: {
+
+    },
+
+    actions: {
+        
+    }
+})
