59ae7f1672ff9c00117a4945d0acfb8cacb40fe9
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-10 16:49:12 +0800

    Add pinia

--- petlify/src/main.js
+++ petlify/src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
@@ -22,5 +23,6 @@ const vuetify = createVuetify({
 
 const app = createApp(App)
 app.use(vuetify)
+app.use(createPinia())
 app.use(router)
 app.mount('#app')
