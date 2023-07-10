f12cd77da584539b2c478f569fadefef369388af
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-10 16:50:23 +0800

    Create pets store

---
+++ petlify/src/stores/pets.js
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+
+export const usePetStores = defineStore({
+    id: 'pets',
+
+    state: () => {
+
+    },
+
+    getters: {
+
+    },
+
+    actions: {
+        
+    }
+})
