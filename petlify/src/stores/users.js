89f7795cfe21e9b1dcedeb1b03539a5520349f06
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-10 16:49:53 +0800

    Create user store

---
+++ petlify/src/stores/users.js
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+
+export const useUserStores = defineStore({
+    id: 'users',
+
+    state: () => {
+
+    },
+
+    getters: {
+
+    },
+
+    actions: {
+        
+    }
+})
