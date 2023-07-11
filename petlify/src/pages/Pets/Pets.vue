ba7e941f20a15a65a715cca1763d433de8f3343a
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-11 20:52:15 +0800

    Create pet gallery

---
+++ petlify/src/pages/Pets/Pets.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+import PetProfileCard from '../../components/PetProfileCard.vue';
+</script>
+
+<template>
+    <Navbar :user-logged-in="true"/>
+
+
+        <div class="flex flex-col min-h-screen min-w-screen overflow-x-scroll  items-center">
+            <label class="text-2xl font-bold">My Pets</label>
+
+            <div class="my-4">
+                <PetProfileCard/>
+            </div>
+        </div>
+
+    <Footer/>
+
+</template>
