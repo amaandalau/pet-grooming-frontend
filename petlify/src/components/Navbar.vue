e0882b65d8f12cb0608a610392de1ab682f299b5
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-11 15:22:12 +0800

    Update navbar

--- petlify/src/components/Navbar.vue
+++ petlify/src/components/Navbar.vue
@@ -1,10 +1,28 @@
 <script setup>
+import { useRouter } from 'vue-router';
 import ButtonNew from './ButtonNew.vue';
-import Buttons from './Buttons.vue';
+import DropdownMenu from './DropdownMenu.vue';
 import Logo from './icons/Logo.vue';
-const props = defineProps({
 
+import { ref } from 'vue';
+
+const props = defineProps({
+    userLoggedIn: {
+        type: Boolean,
+        default: false
+    }
 })
+
+const user = ref({
+    name: 'John Doe',
+    email: 'johndoe@example.com'
+})
+
+const router = useRouter()
+const logout = () => {
+  router.push('/')
+}
+
 </script>
 
 <template>
@@ -26,9 +44,19 @@ const props = defineProps({
                 <ButtonNew class="link" text="About" size="large" elevation="0"/>
             </router-link>
 
-            <router-link to="/signup">
+            <!-- <router-link to="/signup">
+                <ButtonNew text="Sign Up" rounded="sm" size="large"/>
+            </router-link> -->
+
+            <template v-if="userLoggedIn">
+                <DropdownMenu @logout="logout" :name="user.name" :email="user.email"/>
+            </template>
+
+            <template v-else>
+                <router-link to="/signup">
                 <ButtonNew text="Sign Up" rounded="sm" size="large"/>
             </router-link>
+            </template>
         </div>
 
     </div>
