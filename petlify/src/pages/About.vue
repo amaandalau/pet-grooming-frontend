9e08cd66a6d2c6f3a56ac1a072a4e656dbe6ae9b
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-15 18:44:40 +0800

    Add about page

---
+++ petlify/src/pages/About.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue'
+import ButtonNew from '../components/ButtonNew.vue'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+const goToSignUp = () => {
+    router.push('/signup')
+}
+
+</script>
+
+<template>
+    <Navbar/>
+
+    <div class="min-w-screen min-h-screen">
+        <div class="mx-auto my-4 text-center">
+            <label class="font-bold text-3xl">ABOUT</label>
+        </div>
+
+        <div class="m-48 flex items-center justify-center text-center">
+            <p class="font-light text-lg">
+                Experience convenience and peace of mind with our Pet Grooming Appointment Booking System. Find top-notch groomers, book appointments with ease, and receive friendly reminders. Flexible rescheduling options and valuable feedback ensure a seamless grooming experience. Join us today and simplify your pet's grooming routine.
+            </p>
+        </div>
+
+        <div class="mx-48">
+            <ButtonNew text="Sign Up Now" @click="goToSignUp"/>
+        </div>
+
+    </div>
+
+    <Footer/>
+</template>
