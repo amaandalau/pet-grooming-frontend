2a436f25441b3e2ff07f4074ca1d9aa689105553
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-15 16:33:58 +0800

    Update logout function

--- petlify/src/stores/auth.js
+++ petlify/src/stores/auth.js
@@ -1,12 +1,18 @@
 import { defineStore } from 'pinia'
-import Cookies from 'js-cookie'
-
 
 export const useAuthStores = defineStore({
     id: 'auth',
 
     state: () => {
-
+        return {
+            currentUser: {
+                id: null,
+                email: null,
+                role: null
+                // accessToken: null
+            }
+            // currentUser: null
+        }
     },
 
     getters: {
@@ -14,6 +20,34 @@ export const useAuthStores = defineStore({
     },
 
     actions: {
+        async getCurrentUser() {
+            try {
+                const accessToken = localStorage.getItem('access_token')
+
+                if(!accessToken) {
+                    throw new Error('Access token not found')
+                } 
+
+                const response = await fetch('http://localhost:8080/auth/me', {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                })
+
+                if(!response.ok) {
+                    throw new Error('Failed to fetch current user')
+                }
+
+                const userData = await response.json()
+                state.currentUser = userData
+
+                return userData
+            } catch (error) {
+                console.error(error)
+            }
+        },
+
         async registerUser(name, email, password, role) {
             try {
                 const options = {
@@ -44,10 +78,39 @@ export const useAuthStores = defineStore({
                 const data = await response.json()
 
                 const accessToken = data.accessToken
+                this.accessToken = accessToken
 
-                Cookies.set('access_token', accessToken, { path: '/', expires: 7})
+                // Save access token to local storage
+                localStorage.setItem('access_token', accessToken)
 
-                document.cookie = `access_token=${accessToken}; path=/`
+                // Fetch current user
+                const userResponse = await fetch('http://localhost:8080/auth/me', {
+                    method:'GET',
+                    headers: {
+                        Authorization: accessToken
+                    }
+                })
+
+                // const userResponse = await fetch('http://localhost:8080/auth/me', {
+                //     method: 'GET',
+                //     headers: {
+                //         Authorization: `Bearer ${accessToken}`
+                //     }
+                // })
+
+                if (userResponse.ok) {
+                    const userData = await userResponse.json()
+
+                    // Update user info 
+                    this.currentUser = userData
+                    this.userLoggedIn = true
+
+                    const userID = userData.id
+                    console.log('User ID: ', userID)
+                    
+                    return userID
+
+                }
 
                 console.log(data)
                 console.log('User successfully logged in - Thrown from Pinia')
@@ -116,11 +179,19 @@ export const useAuthStores = defineStore({
         },
 
         async logout() {
-            Cookies.remove('access_token', {
-                path: '/'
-            })
+            // Remove access token from local storage
+            localStorage.removeItem('access_token')
+
+            // Reset the currentUser state
+            this.currentUser = {
+                id: null,
+                name: null,
+                email: null,
+                role: null
+            }
 
             console.log('Successfully logged out - Thrown from Pinia')
         }
     }
+
 })
