6e170b1d6ff8ab0467612b611d6deb75c1cc9d28
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-17 19:18:57 +0800

    Update navbar, add navbar menu component

--- petlify/src/components/Navbar.vue
+++ petlify/src/components/Navbar.vue
@@ -1,96 +1,61 @@
 <script setup>
-import { useRouter } from 'vue-router';
-import ButtonNew from './ButtonNew.vue';
-import DropdownMenu from './DropdownMenu.vue';
-import Logo from './icons/Logo.vue';
-
-import { ref } from 'vue';
-import { computed } from 'vue';
-import { useAuthStores } from '../stores/auth';
-import { onMounted } from 'vue';
-import { useUserStores } from '../stores/users';
-
-const props = defineProps({
-    userLoggedIn: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const name = ref(null)
-const email = ref(null)
-let userID = ref(null)
+import { useRouter } from 'vue-router'
+import Logo from './icons/Logo.vue'
+import NavbarMenu from './NavbarMenu.vue'
+import DropdownMenu from './DropdownMenu.vue'
+import ButtonNew from './ButtonNew.vue'
 
 const router = useRouter()
-const authStore = useAuthStores()
-const userStore = useUserStores()
 
-const logout = async () => {
-    await authStore.logout()
+const goToHome = () => {
     router.push('/')
 }
 
-const currentUser = ref(null)
-
-// onMounted(async () => {
-//     if(props.userLoggedIn) {
-//         // currentUser.value = await authStore.getCurrentUser()
-//         await authStore.getCurrentUser()
-//         currentUser.value = authStore.currentUser
-//     }
-// })
-
-const getUserData = async () => {
-    try {
-        const currentUser = await authStore.getCurrentUser()
-        return currentUser
-    } catch (error) {
-        console.error(error)
-        return null
-    }
+const goToAbout = () => {
+    router.push('/about')
 }
 
-const user = computed(() => currentUser.value || {})
-const isLoggedIn = computed(() => props.userLoggedIn && user.value !== null && user.value.id !== null)
-
+const goToSignUp = () => {
+    router.push('/signup')
+}
 
 </script>
 
 <template>
     <div class="bg-transparent flex flex-row items-center justify-between p-4 z-50 drop-shadow-sm">
-
-        <router-link to="/">
-            <div class="flex flex-row justify-start gap-1 hover:cursor-pointer">
-                <Logo/>
-                <label class="font-semibold text-xl hover:cursor-pointer">Petlify</label>
-            </div>
-        </router-link>
-
-        <div class="flex flex-row justify-evenly gap-4">
-            <router-link to="/">
-                <ButtonNew class="link" text="Home" size="large" elevation="0"/>
-            </router-link>
-
-            <router-link to="/about">
-                <ButtonNew class="link" text="About" size="large" elevation="0"/>
-            </router-link>
-
-            <!-- <router-link to="/signup">
-                <ButtonNew text="Sign Up" rounded="sm" size="large"/>
-            </router-link> -->
-
-            <template v-if="props.userLoggedIn">
-                <!-- <DropdownMenu @logout="logout" :user="getUserData()"/> -->
-                <DropdownMenu :user="getUserData()"/>
-            </template>
-
-            <template v-else>
-                <router-link to="/signup">
-                <ButtonNew text="Sign Up" rounded="sm" size="large"/>
-            </router-link>
-            </template>
+        <div @click="goToHome" class="flex flex-row justify-start gap-1 hover:cursor-pointer">
+            <Logo/>
+            <label class="font-semibold text-xl hover:cursor-pointer">Petlify</label>
         </div>
 
-    </div>
+        <div class="flex flex-row items-center justify-end gap-4">
+            <NavbarMenu nav-title="Home" @click="goToHome"/>
 
+            <NavbarMenu nav-title="About" @click="goToAbout"/>
+        </div>
+
+        <!-- If logged in user is owner -->
+        <template v-if="''">
+            <NavbarMenu nav-title="My Pets"/>
+
+            <NavbarMenu nav-title="Appointments"/>
+
+            <DropdownMenu/>
+        </template>
+
+        <!-- If logged in user is groomer -->
+        <template v-if="''">
+            <NavbarMenu nav-title="Upcoming Appointments"/>
+
+            <NavbarMenu nav-title="My Services"/>
+
+            <DropdownMenu/>
+        </template>
+
+        <template v-else>
+            <div class="w-1/6">
+                <ButtonNew @click="goToSignUp" text="Sign Up" rounded="sm" size="large"/>
+            </div>
+        </template>
+    </div>
 </template>
