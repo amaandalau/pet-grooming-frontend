9bb6ae2d085569c2018d21733a9f1a27ec595326
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-19 13:44:48 +0800

    Create custom select dropdown menu

---
+++ petlify/src/components/SelectDropdown.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    label: {
+        type: String
+    },
+    options: {
+        type: Array,
+        required: true
+    }
+})
+
+const selectedValue = ref('')
+</script>
+
+<template>
+    <div class="flex flex-col gap-4">
+        <label class="font-semibold text-black text-left">{{ label }}</label>
+        <select v-model="selectedValue"
+            class="px-5 py-3 border border-black rounded-md focus:border-slate-800 outline-none"
+        >
+            <option value="" disabled selected>{{ label }}</option>
+            <option v-for="option in options" :key="option" :value="option">
+                {{ option }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<style scoped>
+select:focus {
+    border-color: slategrey;
+}
+</style>
