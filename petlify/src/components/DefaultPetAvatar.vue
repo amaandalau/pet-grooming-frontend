a3d0b00f34ba8ee9762206331f1dc0e22ab23430
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-21 09:06:29 +0800

    Created pet appt header

---
+++ petlify/src/components/DefaultPetAvatar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="border-2 border-black rounded-full h-20 w-20 m-8">
+    <!-- Pet Avatar -->
+    <img
+      src="../assets//illustrations/purr-traveler-cat.png"
+      alt=""
+      class="object-contain h-full w-full rounded-full"
+    />
+  </div>
+</template>
---
+++ petlify/src/components/PetApptHeader.vue
@@ -0,0 +1,69 @@
+<script setup>
+import DefaultPetAvatar from './DefaultPetAvatar.vue';
+
+
+const props = defineProps({
+    header: {
+        type: String
+    },
+    subHeader: {
+        type: String
+    },
+    petName: {
+        type: String
+    },
+    petBday: {
+        type: String
+    },
+    petSpecies: {
+        type: String
+    },
+    petBreed: {
+        type: String
+    },
+    petColour: {
+        type: String
+    },
+    petWeight: {
+        type: Number
+    }
+})
+</script>
+
+<template>
+    <div>
+        <div class="py-2">
+            <label class="mx-6 text-xl font-semibold">{{ header }}</label>
+        </div>
+        
+        <!-- Pet Info -->
+        <div class="flex flex-row items-center justify-start">
+            
+            <DefaultPetAvatar/>
+            
+            <div class="w-1/2 border border-blue-500 flex flex-col">
+                <!-- Pet Details -->
+                <div class="flex flex-row items-center justify-between">
+                    <label class="text-xl font-semibold">{{ petName }}</label>
+                    <label>{{ petBday }}</label>
+                </div>
+                
+                <div class="flex flex-row items-center justify-between">
+                    <label>{{ petSpecies }}</label>
+                    <label>{{ petBreed }}</label>
+                </div>
+                
+                <div class="flex flex-row items-center justify-between">
+                    <label>{{ petColour }}</label>
+                    <label>{{ petWeight }} KG</label>
+                </div>
+            </div>
+        </div>
+        
+        <div class="py-2 bg-white drop-shadow-md rounded-lg">
+            <label class="mx-6 text-xl font-semibold">{{ subHeader }}</label>
+        </div>
+    </div>
+
+    <slot></slot>
+</template>
