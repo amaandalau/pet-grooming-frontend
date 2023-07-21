d2ce3775593ef250af1637938d884f78679fba83
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-21 09:07:52 +0800

    update custom dropdown props

--- petlify/src/components/SelectDropdown.vue
+++ petlify/src/components/SelectDropdown.vue
@@ -1,27 +1,44 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const props = defineProps({
     label: {
         type: String
     },
+    subLabel: {
+        type: String
+    },
     options: {
         type: Array,
-        required: true
+        required: false
+    },
+    value: {
+        type: String,
+        default: null
+    },
+    disabled: {
+        type: Boolean,
+        default: false
     }
 })
 
-const selectedValue = ref('')
+const selectedValue = ref(props.value)
+
+watch(() => {
+  // Watch for changes in the value prop and update selectedValue accordingly
+  selectedValue.value = props.value;
+})
 </script>
 
 <template>
-    <div class="flex flex-col gap-1">
-        <label class="mx-1 font-medium text-black text-left">{{ label }}</label>
+    <div class="flex flex-col gap-1 w-auto">
+        <label v-if="label !== ''" class="mx-1 font-medium text-black text-left">{{ label }}</label>
         
         <select v-model="selectedValue"
             class="px-5 py-3 border border-black rounded-md focus:border-slate-800 outline-none"
+            :disabled="disabled"
         >
-            <option value="" disabled selected>{{ label }}</option>
+            <option value="" disabled selected>{{ subLabel }}</option>
             <option v-for="option in options" :key="option" :value="option">
                 {{ option }}
             </option>
