b06a0d7cf13cdec49268b173d859128c26158368
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-24 16:24:07 +0800

    fix routing for groomer

--- petlify/src/components/Navbar.vue
+++ petlify/src/components/Navbar.vue
@@ -13,18 +13,6 @@ const router = useRouter()
 const authStore = useAuthStores()
 const userStore = useUserStores()
 
-const userData = reactive({
-    id: null,
-    role: null
-})
-
-const getUserData = async () => {
-    const user = await authStore.getCurrentUser()
-
-    userData.id = user.id
-    userData.role = user.role
-}
-
 const goToHome = () => {
     router.push('/')
 }
@@ -41,8 +29,16 @@ const goToPetsList = () => {
     router.push(`/${userData.id}/pets`)
 }
 
-const goToApptList = () => {
-    router.push(`/${userData.id}/appointments`)
+const userData = reactive({
+    id: null,
+    role: null
+})
+
+const getUserData = async () => {
+    const user = await authStore.getCurrentUser()
+
+    userData.id = user.id
+    userData.role = user.role
 }
 
 onMounted(() => {
@@ -84,14 +80,14 @@ onMounted(() => {
 
         <!-- If logged in user is groomer -->
         <template v-else-if="userData.role === 'groomer'">
-            <div @click="goToApptList" class="flex flex-row justify-start gap-1 hover:cursor-pointer">
+            <div @click="goToPetsList" class="flex flex-row justify-start gap-1 hover:cursor-pointer">
                 <Logo/>
                 <label class="font-semibold text-xl hover:cursor-pointer">Petlify</label>
             </div>
 
             <div class="flex flex-row items-center justify-end gap-4">
 
-                <NavbarMenu nav-title="Upcoming Appointments" @click="goToApptList"/>
+                <NavbarMenu nav-title="Upcoming Appointments"/>
                 
                 <NavbarMenu nav-title="My Services"/>
                 
