4ffe1208c50fd98e274e65f68ca95c86b74f808c
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-25 18:35:54 +0800

    chg to datepicker

--- petlify/src/pages/Pets/CreatePet.vue
+++ petlify/src/pages/Pets/CreatePet.vue
@@ -5,17 +5,21 @@ import Input from '@/components/Input.vue'
 import InputNew from '@/components/InputNew.vue';
 import ButtonNew from '@/components/ButtonNew.vue'
 
+import VueDatePicker from '@vuepic//vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { useAuthStores } from '@/stores/auth.js'
 import { usePetStores } from '../../stores/pets.js'
 
 const router = useRouter()
+const route = useRoute()
 const authStore = useAuthStores()
 const petStore = usePetStores()
 
 const petName = ref(null)
-const dob = ref(null)
+const petDOB = ref(null)
 const species = ref(null)
 const breed = ref(null)
 const colour = ref(null)
@@ -23,7 +27,7 @@ const weight = ref(null)
 
 const createPet = async () => {
     const petNameValue = petName.value
-    const dateOfBirthValue = dob.value
+    const dateOfBirthValue = petDOB.value
     const speciesValue = species.value
     const breedValue = breed.value
     const colourValue = colour.value
@@ -37,12 +41,19 @@ const createPet = async () => {
     )
 
     router.push(`/${ownerID}/pets`)
+}
+
+const goToPetList = async () => {
+    const currentUser = await authStore.getCurrentUser()
+    const ownerID = currentUser.id
+
+    router.push(`/${ownerID}/pets`)
 } 
 
 </script>
 
 <template>
-    <Navbar :user-logged-in="true"/>
+    <Navbar/>
     <div class="my-4 min-h-screen flex flex-col items-center">
 
         <label class="font-semibold text-2xl">Create New Pet Profile</label>
@@ -55,9 +66,7 @@ const createPet = async () => {
 
         <div class="flex flex-col items-center gap-16">
 
-            <div class="flex flex-row gap-8">
-                <!-- <InputNew label="Pet Name" v-model="petName"/> -->
-                <!-- <InputNew label="Date of Birth" /> -->
+            <div class="flex flex-col items-center justify-between gap-8">
 
                 <Input 
                     label="Pet Name"
@@ -66,55 +75,86 @@ const createPet = async () => {
                     @update:value="petName = $event"
                 />
 
-                <Input 
-                    label="Date of Birth"
-                    placeholder="Date of Birth"
-                    :value="dob" 
-                    @update:value="dob = $event"
-                />
+                <div class="flex flex-col w-full">
+                    <label>Date of Birth</label>
+                    
+                    <VueDatePicker
+                    v-model="petDOB"
+                    :enable-time-picker="false"
+                    :clearable="true"
+                    placeholder="Pet D.O.B"
+                    :close-on-auto-apply="true"
+                    :show-now-button="false"
+                    width="400"
+                    ></VueDatePicker>
+
+                    <p class="mt-1 font-extralight text-xs italic">*Date of Birth cannot be changed after creating</p>
+                </div>
 
             </div>
 
             <div class="flex flex-row gap-8">
-                <!-- <InputNew label="Species"/>
-                <InputNew label="Breed"/> -->
 
-                <Input
+                <div class="flex flex-col gap-1">
+                    <Input
                     label="Species"
                     placeholder="Species"
                     :value="species"
                     @update:value="species = $event"
-                />
+                    />
 
-                <Input
+                    <p class="mt-1 font-extralight text-xs italic">*Species cannot be changed after creating</p>
+
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <Input
                     label="Breed"
                     placeholder="Breed"
                     :value="breed"
                     @update:value="breed = $event"
-                />
+                    />
+
+                    <p class="mt-1 font-extralight text-xs italic">*Breed cannot be changed after creating</p>
+
+                </div>
             </div>
 
             <div class="flex flex-row gap-8">
-                <!-- <InputNew label="Colour"/>
-                <InputNew label="Weight (in KG)"/> -->
 
-                <Input
+                <div class="flex flex-col gap-1">
+                    <Input
                     label="Colour"
                     placeholder="Colour"
                     :value="colour"
                     @update:value="colour = $event"
-                />
+                    />
 
-                <Input
+                    <p class="mt-1 font-extralight text-xs italic">*Colour cannot be changed after creating</p>
+
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <Input
                     label="Weight (in KG)"
                     placeholder="Weight (in KG)"
                     :value="weight"
                     @update:value="weight = $event"
-                />
+                    />
+
+                    <p class="mt-1 font-extralight text-xs italic">‎</p>
+                </div>
             </div>
 
 
-            <ButtonNew text="Create" rounded="sm" @click="createPet"/>
+            <div class="flex flex-col items-center justify-between w-full gap-4">
+                <ButtonNew text="Create" class="default" rounded="sm" @click="createPet"/>
+
+                <label @click="goToPetList"
+                        class="font-light text-red-600 hover:underline hover:cursor-pointer">
+                        Cancel
+                </label>
+            </div>
         </div>
     </div>
     <Footer/>
