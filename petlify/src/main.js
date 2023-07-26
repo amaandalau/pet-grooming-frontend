dc5d9e2dae133fb78ccc75987dd870d7b9e8e812
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-26 13:33:10 +0800

    import vuedatepicker

--- petlify/src/main.js
+++ petlify/src/main.js
@@ -13,6 +13,10 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+
+
 const vuetify = createVuetify({
   components,
   directives,
@@ -23,6 +27,7 @@ const vuetify = createVuetify({
 
 const app = createApp(App)
 app.use(vuetify)
+app.component('VueDatePicker', VueDatePicker)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
