abe8a16a10fc38d7b06bf9e5ca93604c51b03892
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-26 12:12:02 +0800

    update footer name

--- petlify/src/components/Footer.vue
+++ petlify/src/components/Footer.vue
@@ -5,7 +5,7 @@
   <footer class="bottom-0 left-0 right-0 p-4 bg-black">
         <div class="text-center">
             <label class="text-sm font-normal text-white">
-              &copy; {{ new Date().getFullYear() }} All rights reserved | @amaandalau
+              &copy; {{ new Date().getFullYear() }} All rights reserved | Petlify
             </label>
         </div>
   </footer>
